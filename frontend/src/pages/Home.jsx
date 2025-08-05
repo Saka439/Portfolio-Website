4b8ad6d8e0c392baa495b8abf8b71427c615d7c4
Saka439 <ndzana-missia.saka@polymtl.ca>
2025-08-05 14:45:15 -0400

    optimisation2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
     return (
         <div className="home">
-            <h1>Bienvenue sur mon Portfolio</h1>
-            <p>Je suis étudiant en 3ème année de génie informatique à Polytechnique Montréal, 
-                passionné par le développement web et logiciel. En tant qu'apporteur de solutions 
-                créatives, je recherche activement un stage pour mettre en pratique mes compétences 
-                techniques et contribuer à des projets innovants.</p>
+            <div className="hero-section">
+                <img 
+                    src="/Galax.jpg" 
+                    className="profile-image"
+                />
+                <div className="hero-content">
+                    <h1>Bienvenue sur mon Portfolio</h1>
+                    <p>Je suis étudiant en 3ème année de génie informatique à Polytechnique Montréal, 
+                        passionné par le développement web et logiciel.</p>
+                    <p>En tant qu'apporteur de solutions 
+                       créatives, je recherche activement un stage pour mettre en pratique mes compétences 
+                       techniques et contribuer à des projets innovants.</p>
+                </div>
+            </div>
         </div>
+
     );
 };
 
